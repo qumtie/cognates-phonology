8cd2e74ff8d19a2030aebd760778b7d5cea832f3
qumtie <38039202+qumtie@users.noreply.github.com>
2018-04-03 15:09:33 +0100

    Update m1012graphonolev.sh

--- src/phonolev/m1012graphonolev.sh
+++ src/phonolev/m1012graphonolev.sh
@@ -5,7 +5,7 @@
 # argv[3] = target language code (must be the same as the character feature language codes ... )
 # argv[4] = 'Debug' to create a debug file, 'None' not to create a debug file; the Debug logs the script, recording the history of matches, matrices for Levenshtein feature-based calculations and comparisons and log of matched features
 # argv[5] = use transliteration table for baseline Levenshtein? None if not; if yes, then provide the name of the file in tsv format, character \t replacement character
-# argv[6] = the list of phonetic feature tables used, separated by coma (no space), in the tsv format; first field = character; second field = list of languages; third field = tab separated phonological features and their values for this character
+# argv[6] = the list of phonological feature tables used, separated by coma (no space), in the tsv format; first field = character; second field = list of languages; third field = tab separated phonological features and their values for this character
 # examples do processing for closely related languages (uk, ru) and for distant languages (en, uk) (uk, or ua -- Ukrainian)
 python m1012graphonolev.py m1012graphonolev-uk-ru-in.txt ua ru Debug None m1012graphonolev-phonetic-features.tsv,m1012graphonolev-phonetic-features0.tsv  >m1012graphonolev-uk-ru-out.txt
 python m1012graphonolev.py m1012graphonolev-en-uk-in.txt en ua Debug m1012graphonolev-phonetic-trans-lat2cyr.txt m1012graphonolev-phonetic-features.tsv,m1012graphonolev-phonetic-features0.tsv  >m1012graphonolev-en-uk-out.txt
@@ -28,3 +28,5 @@ python m1012graphonolev.py m1012graphonolev-en-uk-in.txt en ua Debug m1012grapho
 # Lev1 = phonological Levenshtein distance, with insertion cost 1
 # Lev1Norm = the same, normalised for length
 
+# two lines are generated for each word pair in the input file -- for each phonological feature set, specified in argv[6] = the list of phonological feature tables
+# (the first one is the hierarcical (best performance); the second one is plain vector of features (over-generates on larger search spaces)
